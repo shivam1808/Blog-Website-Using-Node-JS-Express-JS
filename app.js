b6c45b6342d0b0b122387b22622d68a5ad4a80c3
Shivam Gupta <shivamguptasg1808@gmail.com>
2022-04-06 23:17:28 +0530

    Added Image Section in Post

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const router = require('./router');
 
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const aboutContent = "A blog is often the product of a content management system. Whether a blog patron merely wants to voice their thoughts or a business wants to write persuasive copy, a blog is the place to be. Though a blog’s primary job is displaying words on the client-side, the server-side is essential to archiving blog posts as they accumulate. ";
+const contactContent = "Mail Id: shivamguptasg1808@gmail.com";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
@@ -114,6 +116,8 @@ app.get("/blogs/:blogName/posts/:postName", function(req, res){
   });
 
 });
+
+app.use('/upload', router);
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
